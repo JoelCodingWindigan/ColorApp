3aed7160b4e446d7add407823eed4814cb41af70
JoelCodingWindigan <mannavavamsi03@gmail.com>
2024-10-21 09:54:40 -0700

    Initial Commit

---
+++ ColorApp/ColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppApp.swift
+//  ColorApp
+//
+//  Created by Vamsi Mannava on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
